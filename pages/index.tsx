07f5f9f398d55c686e57d43bf224e66a488f5588
Andres Guerrero <andresmgsl@gmail.com>
2019-08-20 18:02:54 +0200

    :star: First Commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+export default class extends React.Component {
+  render() {    
+    return (
+      <div>
+        <h1>De Maracay pal mundo..</h1>
+        
+        <h3>Vamos a ver que tal..</h3>
+      </div>
+    )
+  }
+}
