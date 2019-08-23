70c25b782cc90fff55a26f2b65aa26e32712f716
Andres Guerrero <andresmgsl@gmail.com>
2019-08-23 02:15:29 +0200

    :scissors: Preload images while animatedLogo is runing. Avoid wait for it later

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,11 @@ import styled from 'styled-components';
 declare var TimelineMax: any;
 declare var Power2: any;
 
+const PreloadImg = styled.img`
+  display: none;
+  opacity: 0;
+`
+
 const IndexContainer = styled.div`
   width: 100%;
   height: 100%;
@@ -52,6 +57,18 @@ class Index extends React.Component<IProps, IState> {
     return (
         <IndexContainer id="main-index-container">
           
+        {/** Preload image while animateLogo is runing */}
+        <PreloadImg src="/static/images/Home/6db0215-720.jpg"></PreloadImg>
+        <PreloadImg src="/static/images/Home/25ae41f-480.jpg"></PreloadImg>
+        <PreloadImg src="/static/images/Home/96e5535-720.jpg"></PreloadImg>
+        <PreloadImg src="/static/images/Home/390c2a9-480.jpg"></PreloadImg>
+        <PreloadImg src="/static/images/Home/958e114-480.jpg"></PreloadImg>
+        <PreloadImg src="/static/images/Home/86174c1-720.jpg"></PreloadImg>
+        <PreloadImg src="/static/images/Home/725127e-720.jpg"></PreloadImg>
+        <PreloadImg src="/static/images/Home/3084359-480.jpg"></PreloadImg>
+        <PreloadImg src="/static/images/Home/ecf98a4-480.jpg"></PreloadImg>
+
+
          {this.state.showAnimatedLogo && <AnimatedLogo 
                                             lgTitle={title} 
                                             lgSubTitle={subTitle}
